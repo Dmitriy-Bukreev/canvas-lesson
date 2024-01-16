b702760ec180989e46d0ef519e135d47cdb28729
Dmitriy-Bukreev <dmitriy.bukreev.job@gmail.com>
2024-01-17 02:48:18 +0300

    Fix

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,8 @@
 export default function generateArray(start = 0, stop = null, step = 1) {
-	return [0, 1, 2, 3, 4];
+	const [actualStart, actualStop] = stop === null ? [0, start] : [start, stop];
+	  const actualLength = (actualStop - actualStart) / step;
+	  return Array.from(
+	    { length: actualLength },
+	    (v, k) => (k + actualStart) * step,
+	  );
 }
