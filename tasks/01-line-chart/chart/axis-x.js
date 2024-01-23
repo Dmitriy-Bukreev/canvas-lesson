ff339a9c2d87c2707bff7f0eead1cf7817d5c96b
Dmitriy-Bukreev <dmitriy.bukreev.job@gmail.com>
2024-01-23 23:52:29 +0300

    Solution for task 3

---
+++ tasks/01-line-chart/chart/axis-x.js
@@ -0,0 +1,35 @@
+import Axis from './axis';
+
+class AxisX extends Axis {
+  constructor(...args) {
+    super(...args);
+    this.generateRenderParams(this.canvas.width);
+  }
+
+  render() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(this.canvas.width, 0);
+    this.ctx.stroke();
+
+    this.ctx.textAlign = 'right';
+    this.ctx.textBaseline = this.mirroredLabelLocation ? 'top' : 'bottom';
+    this.labels.forEach((label, index) => {
+      if (label === this.axisStart) return;
+      const pos = this.dashInterval * index + this.dashStart;
+      this.ctx.beginPath();
+      this.ctx.moveTo(pos, -this.dashLength / 2);
+      this.ctx.lineTo(pos, this.dashLength / 2);
+      this.ctx.stroke();
+
+      const textMetrics = this.ctx.measureText(Math.abs(label));
+      const textX = pos + textMetrics.width / 2;
+      const textY = this.mirroredLabelLocation
+        ? this.labelOffset
+        : this.labelOffset;
+      this.ctx.fillText(label, textX, textY);
+    });
+  }
+}
+
+export default AxisX;
---
+++ tasks/01-line-chart/chart/axis-y.js
@@ -0,0 +1,35 @@
+import Axis from './axis';
+
+class AxisY extends Axis {
+  constructor(...args) {
+    super(...args);
+    this.labels.reverse();
+    this.generateRenderParams(this.canvas.height);
+  }
+
+  render() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(0, this.canvas.height);
+    this.ctx.stroke();
+
+    this.ctx.textBaseline = 'middle';
+    this.ctx.textAlign = this.mirroredLabelLocation ? 'right' : 'left';
+    this.labels.forEach((label, index) => {
+      if (label === this.axisStart) return;
+      const pos = this.dashInterval * index + this.dashStart;
+      this.ctx.beginPath();
+      this.ctx.moveTo(-this.dashLength / 2, pos);
+      this.ctx.lineTo(this.dashLength / 2, pos);
+      this.ctx.stroke();
+
+      const textX = this.mirroredLabelLocation
+        ? -this.labelOffset
+        : this.labelOffset;
+      const textY = pos;
+      this.ctx.fillText(label, textX, textY);
+    });
+  }
+}
+
+export default AxisY;
