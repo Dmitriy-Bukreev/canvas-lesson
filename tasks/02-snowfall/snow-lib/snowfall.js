f23dbff30382c1aae5d961dc96f4d8f448596d96
Dmitriy-Bukreev <dmitriy.bukreev.job@gmail.com>
2024-02-09 15:55:55 +0300

    Solution for task 7

--- tasks/02-snowfall/snow-lib/snowfall.js
+++ tasks/02-snowfall/snow-lib/snowfall.js
@@ -24,6 +24,7 @@ class Snowfall {
     this.canvas.className = 'canvas-snowfall';
     /** @type {CanvasRenderingContext2D} */
     this.ctx = this.canvas.getContext('2d');
+    this.canvas.className = 'canvas-snowfall';
     /** @type {Snowflake[]} */
     this.snowflakes = [];
     /** @type {ResizeObserver | null} */
@@ -58,6 +59,61 @@ class Snowfall {
   } = {}) {
     this.snowflakeSettings = { amount, minSize, maxSize, fallSpeed };
   }
+
+  add() {
+    while (this.snowflakes.length < this.snowflakeSettings.amount) {
+      const y = randInt(-this.canvas.height, 0);
+      const x = randInt(0, this.canvas.width);
+      const radius = randInt(
+        this.snowflakeSettings.minSize,
+        this.snowflakeSettings.maxSize,
+      );
+      this.snowflakes.push(new Snowflake(this.ctx, x, y, radius));
+    }
+  }
+
+  update() {
+    this.snowflakes.forEach((snowflake) => {
+      snowflake.moveBy(0, this.getSnowflakeSpeed(snowflake));
+    });
+  }
+
+  remove() {
+    this.snowflakes = this.snowflakes.filter((snowflake) =>
+      this.isVisible(snowflake),
+    );
+  }
+
+  /**
+   * Calculates snowflake speed
+   * @param {Snowflake} snowflake
+   * @returns {number}
+   */
+  getSnowflakeSpeed(snowflake) {
+    return (
+      (snowflake.radius /
+        (this.snowflakeSettings.maxSize - this.snowflakeSettings.minSize)) *
+      this.snowflakeSettings.fallSpeed
+    );
+  }
+
+  /**
+   * Checks if snowflake is within canvas boundaries
+   * @param {Snowflake} snowflake
+   * @returns {boolean}
+   */
+  isVisible(snowflake) {
+    return snowflake.y - snowflake.radius < this.canvas.height;
+  }
+
+  lifecycle = () => {
+    this.add();
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.update();
+    this.remove();
+
+    requestAnimationFrame(this.lifecycle);
+  };
 }
 
 export default Snowfall;
